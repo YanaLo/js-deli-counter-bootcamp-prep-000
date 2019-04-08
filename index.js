c7e1cea85f8cd00ddcbd39e78ea35fc9d390ddc3
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-04-08 17:26:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function takeANumber(theLine, theName){
+  theLine.push(theName)
+  return `Welcome, ${theName}. You are number ${theLine.length} in line.`
+}
+
+  function nowServing(theLine){
+    // returns the first person in laine
+    // then removes them from that line
+    // if nobody in line return the message
+    if(Array.isArray(theLine) && )
+  }
